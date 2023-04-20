2240b678e28a70298771bfcbeebb7ad1f4472b57
Andrea Falcon <45632327+AndreaFalconCortes@users.noreply.github.com>
2023-04-20 13:51:15 -0400

    Update NetworkFiles.py

--- PipelineProject/NetworkFiles.py
+++ PipelineProject/NetworkFiles.py
@@ -13,7 +13,7 @@ def clean_df(df):
     
     for i in df.columns.values:
         
-        if((i == 'ChromA') | (i == 'ChromB') | (i == 'OG')):
+        if((i == 'ChromA') | (i == 'ChromB') | (i == 'OGA') | (i == 'OGB')):
             
             df[i] = df[i].str.replace('A', '')
             df[i] = df[i].str.replace('B', '')
@@ -43,27 +43,34 @@ for j in tsv:
                 fields = line.strip().split('\t')
                 data.append(fields)
                 
-    df = pd.DataFrame(data, columns=['ChromA', 'OG', 'GeneIndexA', 'GeneIndexAA', 'ChromB', 'OGB', 'GeneIndexB', 'GeneIndexBB', 'Score0', 'Score'])
+    df = pd.DataFrame(data, columns=['ChromA', 'OGA', 'GeneIndexA', 'GeneIndexAA', 'ChromB', 'OGB', 'GeneIndexB', 'GeneIndexBB', 'Score0', 'Score'])
     
     if(len(df) != 0):
     
-        df = df.drop(['GeneIndexAA', 'GeneIndexBB', 'OGB' ,'Score0'], axis = 1)
+        df = df.drop(['GeneIndexAA', 'GeneIndexBB' ,'Score0', 'Score'], axis = 1)
+        
+        df = df.drop_duplicates()
                 
         df_split = df.loc[df['ChromA'].str.contains('B')]        
         
         diff_df = df.merge(df_split, how='outer', indicator=True)
         diff_df = diff_df.loc[diff_df['_merge'] == 'left_only'].drop('_merge', axis=1)
         
+        df_split.columns = ['ChromB', 'OGB', 'GeneIndexB' ,'ChromA', 'OGA', 'GeneIndexA']
+        df_split = df_split[['ChromA', 'OGA', 'GeneIndexA', 'ChromB', 'OGB', 'GeneIndexB']]  
+        
         df_unsplit = pd.concat([diff_df, df_split], axis=0)
         df_unsplit = df_unsplit.reset_index(drop=True)
         
         df_unsplit = clean_df(df_unsplit)
         
+        df_unsplit['ChromA'] = df_unsplit.apply(lambda row: str(j.split('-')[0] + '_Ch-' + row['ChromA']), axis=1)
+        df_unsplit['ChromB'] = df_unsplit.apply(lambda row: str(j.split('-')[1] + '_Ch-' + row['ChromB']), axis=1)
+             
+        df_unsplit['GeneA'] = df_unsplit.apply(lambda row: str(j.split('-')[0] + '_G-' + row['GeneIndexA']), axis=1)
+        df_unsplit['GeneB'] = df_unsplit.apply(lambda row: str(j.split('-')[1] + '_G-' + row['GeneIndexB']), axis=1)
         
-        df_unsplit['GeneA'] = df_unsplit.apply(lambda row: str(j.split('-')[0] + '_G' + row['GeneIndexA']) + '_Ch' + row['ChromA'], axis=1)
-        df_unsplit['GeneB'] = df_unsplit.apply(lambda row: str(j.split('-')[1] + '_G' + row['GeneIndexB']) + '_Ch' + row['ChromB'], axis=1)
-        
-        df_net = df_unsplit[['GeneA', 'GeneB', 'OG', 'Score']]
+        df_net = df_unsplit[['ChromA','GeneA', 'OGA' ,'ChromB','GeneB', 'OGB']]
         
         name_file = j.split('-')[0] + '-' + j.split('-')[1] + '.csv' 
         
